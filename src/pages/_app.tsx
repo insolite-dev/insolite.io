d845d36711270591669d017905ee59c3834d42be
Onurhan1337 <onurhandtr@gmail.com>
2023-04-02 11:19:32 +0300

    feat: added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css"
+import { Analytics } from "@vercel/analytics/react"
 import Header from "components/header"
 import Footer from "components/footer"
 import type { AppProps } from "next/app"
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <footer className="bg-[#111111] py-6">
                 <Footer />
             </footer>
+            <Analytics />
         </div>
     )
 }
