2e08b8a31dd8d43488279ae9f6ec8e0b1f543d0d
Ismael Sh <me@theiskaa.com>
2023-10-24 21:56:23 +0400

    fix: remove experimental edge from next js config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['firebasestorage.googleapis.com'],
-    },
-	experimental: { runtime: 'edge'}
+    }
 }
 
 module.exports = nextConfig
